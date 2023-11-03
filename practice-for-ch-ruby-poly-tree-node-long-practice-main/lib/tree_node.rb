b4337d77ffaeeca45f5c9bddc2afdabe7ec61bb5
Peter Nolan <peter.l.nolan@gmail.com>
2023-11-03 14:08:28 -0400

    Add initialize and parent setter methods as well as reader methods to the PolyTreeNode class

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,29 @@
+require "byebug"
 class PolyTreeNode
+
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        if node != nil
+            unless !parent
+                parent.children.each_with_index do |child, i|
+                    parent.children = parent.children[0...i] + parent.children[i + 1..-1] if child == self
+                end
+            end
+            @parent = node
+            unless parent.children.include?(self)
+                parent.children << self
+            end
+        else
+            nil
+        end
+    end
+
   
 end
