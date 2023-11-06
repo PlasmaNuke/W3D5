26887f59603db3e732f1233ae86a8bf0d2defb9d
Eric Lee <erlee92@yahoo.com>
2023-11-06 15:30:43 -0500

    add trace_path_back method & find_path

--- knights_project/knight_pathfinder.rb
+++ knights_project/knight_pathfinder.rb
@@ -2,7 +2,8 @@ require "byebug"
 require_relative "tree_node.rb"
 
 class KnightPathFinder
-    attr_reader :start_position
+    attr_reader :start_position, :considered_positions
+
 
     def self.valid_moves(pos)
         x, y = pos
@@ -13,8 +14,8 @@ class KnightPathFinder
         [x - 2, y + 1], [x + 1, y - 2],
         [x - 2, y - 1], [x - 1, y - 2]
         ]
-        potential_positions.select{|pos|pos[0] >= 0 && pos[0] <= 8 && pos[
-        1] >= 0 && pos[1] <= 8}
+        potential_positions.select{|pos|pos[0] >= 0 && pos[0] < 8 && pos[
+        1] >= 0 && pos[1] < 8}
     
     end
 
@@ -22,19 +23,20 @@ class KnightPathFinder
         @root_node = PolyTreeNode.new(position)
         @considered_positions = [position]
         build_move_tree
+        # self.new_move_positions(position)
     end
 
     def new_move_positions(pos)
        val_moves = KnightPathFinder.valid_moves(pos).reject { |pos| @considered_positions.include? (pos) }
 
-       @considered_positions.push(val_moves)
+       @considered_positions += val_moves
        val_moves
     end
 
     def build_move_tree
         queue = [@root_node]
 
-        debugger
+        # debugger
 
         until queue.empty?
 
@@ -46,7 +48,24 @@ class KnightPathFinder
                 queue.push(new_move)
             end
         end
+    end
 
+    def trace_path_back(pos)
+        current = pos
+        path = []
+
+        while current != @root_node
+            path << current.value
+            current = current.parent
+        end
+        path << @root_node.value
+        path.reverse
+
+    end
+
+    def find_path(end_pos)
+        end_node =  @root_node.dfs(end_pos)
+        trace_path_back(end_node)
     end
 
 
